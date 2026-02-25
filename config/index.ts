d8a629d01da8aa305379f64924ab599f1ae57344
Aman Ali Qadri <154038673+AmanAliMon@users.noreply.github.com>
2026-02-25 19:47:15 +0500

    Add files via upload

---
+++ config/index.ts
@@ -0,0 +1,101 @@
+export default {
+  /**
+   * Nextra metadata configuration
+   * @see https://nextra.vercel.app/docs/metadata
+   */
+  metadata: {
+    title: {
+      default: 'Fubam (Functions Based Markup)',
+      template: '%s | Fubam',
+    },
+    description: 'Fubam is a modern web templating engine with functional paradigm tailored for high complexity templating!',
+    metadataBase: new URL('https://fubam.mdadsolutions.com/'),
+    keywords: [
+      'Fubam',
+      'Functions based markup',
+      'Fubam.py',
+      'Fubam library',
+    'Python web templating',
+      'Functional templating paradigm',
+      'Static Site Generator',
+    ],
+    generator: 'Next.js',
+    applicationName: 'FubamDocs',
+    appleWebApp: {
+      title: 'Fubam',
+    },
+    openGraph: {
+      // https://github.com/vercel/next.js/discussions/50189#discussioncomment-10826632
+      url: './',
+      siteName: 'Fubam',
+      locale: 'en_US',
+      type: 'website',
+    },
+    other: {
+      'msapplication-TileColor': '#fff',
+    },
+    twitter: {
+      site: 'https://fubam.mdadsolutions.com/',
+    },
+    alternates: {
+      // https://github.com/vercel/next.js/discussions/50189#discussioncomment-10826632
+      canonical: './',
+    },
+  },
+  /**
+   * Nextra Layout component configuration
+   */
+  nextraLayout: {
+    docsRepositoryBase: 'https://github.com/gfazioli/next-app-nextra-template/tree/main/app/docs/',
+    sidebar: {
+      defaultMenuCollapseLevel: 1,
+    },
+  },
+  /**
+   * Main Layout head configuration
+   */
+  head: {
+    mantine: {
+      defaultColorScheme: 'dark',
+      nonce: '8IBTHwOdqNKAWeKl7plt8g==',
+    },
+  },
+  /**
+   * GitHub API configuration
+   * @see https://docs.github.com/en/rest/reference/repos#releases
+   *
+   * The GitHub API token is optional for rate limiting.
+   * If you want to use it, create a personal access token with the `repo` scope.
+   *
+   * This information is used to fetch the releases from the GitHub API.
+   */
+  gitHub: {
+    repo: 'gfazioli/next-app-nextra-template',
+    apiUrl: 'https://api.github.com',
+    releasesUrl: 'https://api.github.com/repos/gfazioli/next-app-nextra-template/releases',
+  },
+
+  /**
+   * Release notes configuration
+   * This is used to link the release notes in the app.
+   */
+  releaseNotes: {
+    url: 'https://github.com/gfazioli/next-app-nextra-template/releases',
+    maxReleases: 10,
+  },
+
+  /**
+   * Search configuration (for pagefind)
+   * This is used to configure the search engine API.
+   * @see /app/api/search/route.ts
+   */
+  search: {
+    queryKeyword: 'q',
+    minQueryLength: 3,
+    limitKeyword: 'limit',
+    defaultMaxResults: 5,
+    excerptLengthKeyword: 'excerptLength',
+    defaultExcerptLength: 30,
+    defaultLanguage: 'en',
+  },
+} as const;
---
+++ content/_meta.ts
@@ -0,0 +1,29 @@
+export default {
+  index: 'Introduction',
+  '##': {
+    type: 'separator',
+  },
+  api: '',
+
+  'getting-started':'',
+  // 'installation':'',
+  'core-concepts':'',
+  'writing-fubam':'',
+  'components':'',
+  layouts:'',
+  'fubam-cli':'',
+  markdown: '',
+  mantine: '',
+  
+//  '###': {
+  //  type: 'separator',
+    // title: 'My Items', // Title is optional
+  //},
+  // versioning: {
+  //   theme: {
+  //     toc: false,
+  //   },
+  // },
+  // 'release-notes': '',
+};
+ 
